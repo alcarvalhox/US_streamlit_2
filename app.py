0f8a09636fc4f29fb6fcdeeb0b00728a0feb6efd
alcarvalhox <59361388+alcarvalhox@users.noreply.github.com>
2026-05-07 07:20:48 -0300

    Update title text for defect detection page

--- app.py
+++ app.py
@@ -268,7 +268,7 @@ def main():
             st.warning("⚠️ Logo não encontrada.")
             
     with col_titulo:
-        st.markdown("<h1 style='text-align: center; margin-top: -10px;'>Detecção Automática de Defeitos de Inspeções de US</h1>", unsafe_allow_html=True)
+        st.markdown("<h1 style='text-align: center; margin-top: -10px;'>Detecção Automática de Defeitos em Inspeções de US</h1>", unsafe_allow_html=True)
         
     with col_veiculo:
         try:
